a7b308d90c0a8a03f1254ba67bc580ea01fc372d
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-01-27 10:46:43 +0530

    fix issue when decoding an empty string

--- tests/utils.js
+++ tests/utils.js
@@ -7,6 +7,12 @@ Tinytest.add('Utils - encode decode', function(test) {
   test.equal(decoded.date.getTime(), data.date.getTime());
 });
 
+Tinytest.add('Utils - decode empty', function(test) {
+  var str = ""
+  var decoded = InjectData._decode(str);
+  test.equal(decoded, null);
+});
+
 Tinytest.add('Utils - encode decode special chars', function(test) {
   var data = {special: "#://"};
   var str = InjectData._encode(data);
